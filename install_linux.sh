49b734ef0888575b3472df8c8b0c42a64f8a946e
Yang Zhirui <136310248+yang6world@users.noreply.github.com>
2024-02-29 22:05:35 +0800

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -4,12 +4,12 @@ pluginsDir='/opt/LiteLoader/plugins'
 
 
 echo "正在拉取最新版本的仓库..."
-rm -rf LiteLoader
+rm -rf /root/nonebot/LiteLoader
 git clone https://github.com/LiteLoaderQQNT/LiteLoaderQQNT.git LiteLoader
 
 # 移动到安装目录
 echo "拉取完成，正在安装LiteLoader..."
-sudo cp -f ./LiteLoader/src/preload.js /opt/QQ/resources/app/application/preload.js
+sudo cp -f /root/nonebot/LiteLoader/src/preload.js /opt/QQ/resources/app/application/preload.js
 
 # 如果目标目录存在且不为空，则先备份处理
 if [ -e "/opt/LiteLoader" ]; then
@@ -22,7 +22,7 @@ if [ -e "/opt/LiteLoader" ]; then
 fi
 
 # 移动LiteLoader
-sudo mv -f LiteLoader /opt
+sudo mv -f /root/nonebot/LiteLoader /opt
 
 # 如果LiteLoader_bak中存在plugins文件夹，则复制到新的LiteLoader目录
 if [ -d "/opt/LiteLoader_bak/plugins" ]; then
